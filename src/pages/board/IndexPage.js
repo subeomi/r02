004ff1f68784388ed19ea666ac5ce04e35ad5879
subeomi <tnqjarnqja@gmail.com>
2023-07-10 17:49:20 +0900

    0710

--- src/pages/board/IndexPage.js
+++ src/pages/board/IndexPage.js
@@ -4,7 +4,7 @@ import BasicLayout from "../../layouts/BasicLayout";
 const IndexPage = () => {
     return ( 
         <BasicLayout>
-            <div className="p-4 h-screen bg-white flex justify-center bg-opacity-25 backdrop-blur-sm">
+            <div className="p-4 h-full bg-amber-50 flex justify-center bg-opacity-25 backdrop-blur-sm">
                 <div className="w-[40vw]">
                 <Outlet></Outlet>
                 </div>
